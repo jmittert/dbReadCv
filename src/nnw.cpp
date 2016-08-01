7080fcb36b3aa3bc4723822eda85f69216d1039f
Jason Mittertreiner <jmittert@uwaterloo.ca>
2016-08-01 00:26:42 -0700

    Working machine learning!!!

--- src/nnw.cpp
+++ src/nnw.cpp
@@ -156,8 +156,9 @@ int main(int argc, char **argv)
   pqxx::work txn(c);
 
   int numImages = txn.exec("SELECT count(*) FROM images")[0][0].as<int>(0);
+
   Mat training_inputs = Mat(numImages, 7, CV_32F);
-  Mat training_outputs = Mat(numImages, 6, CV_32F);
+  Mat training_outputs = Mat(numImages, 2, CV_32F);
   cout << "Reading data..." << endl;
   Ptr<ml::ANN_MLP> brain;
   // check if the data already exists
@@ -168,7 +169,7 @@ int main(int argc, char **argv)
   }
   else
   {
-    pqxx::result res = txn.exec("SELECT image, a1, a2, b1, b2, lpwm, rpwm FROM images");
+    pqxx::result res = txn.exec("SELECT image, lpwm, rpwm FROM images");
     Mat img;
     for (int i = 0; i < numImages; ++i)
     {
@@ -180,78 +181,76 @@ int main(int argc, char **argv)
       Point pix2 = firstBlackRight(img);
       Point highest = highestLine(img);
       int wperc = whitePercent(img);
+      training_inputs.at<float>(i,0) = pix.x;
+      training_inputs.at<float>(i,1) = pix.y;
+      training_inputs.at<float>(i,2) = pix2.x;
+      training_inputs.at<float>(i,3) = pix2.y;
+      training_inputs.at<float>(i,4) = highest.x;
+      training_inputs.at<float>(i,5) = highest.y;
+      training_inputs.at<float>(i,6) = wperc;
 
-      int* ptr = training_inputs.ptr<int>(i);
-      ptr[0] = pix.x;
-      ptr[1] = pix.y;
-      ptr[2] = pix2.x;
-      ptr[3] = pix2.y;
-      ptr[4] = highest.x;
-      ptr[5] = highest.y;
-      ptr[6] = wperc;
-
-      ptr = training_outputs.ptr<int>(i);
-      for (int j = 0; j < 6; ++j) {
-        ptr[j] = res[i][j+1].as<int>();
-      }
+      training_outputs.at<float>(i,0) = res[i][1].as<float>();
+      training_outputs.at<float>(i,1) = res[i][2].as<float>();
     }
 
-    TermCriteria criteria = TermCriteria(TermCriteria::MAX_ITER + TermCriteria::EPS, 1000, 0.0001f);
-
     brain = ANN_MLP::create();
-    Mat layers = (Mat_<int>(1,3) << 7, 6, 6);
-
+    Mat layers = (Mat_<int>(1,3) << 7, 7, 2);
+    
+    // You have to set the activationFunction AFTER the Layer sizes else
+    // you get NaN in the outputs
+    // WTF: http://stackoverflow.com/questions/36871277/opencv-3-1-ann-predict-returns-nan
     brain->setLayerSizes(layers);
-    brain->setTermCriteria(criteria);
+    brain->setActivationFunction(cv::ml::ANN_MLP::SIGMOID_SYM);
 
     Ptr<TrainData> data = TrainData::create(training_inputs, SampleTypes::ROW_SAMPLE, training_outputs);
-    //data->setTrainTestSplitRatio(0.75);
-
+    data->setTrainTestSplitRatio(0.75);
     brain->train(data);
-    //Mat output;
-    //float err = brain->calcError(data, true, output);
-    //cout << "Err: " << err << endl;
+
+    Mat output;
+    float err = brain->calcError(data, true, output);
+    cout << "Err: " << err << endl;
     brain->save("ml.out");
   }
 
+  // Make some predictions
+  std::stringstream ss;
+	pqxx::result res = txn.exec("SELECT image, lpwm, rpwm FROM images");
+  Mat inputs = Mat(numImages, 7, CV_32F);
   for (int i = 0; i < numImages; ++i)
   {
-    std::stringstream ss;
-    ss << "SELECT image, a1, a2, b1, b2, lpwm, rpwm FROM images WHERE id=" << i+1;
-    pqxx::result res = txn.exec(ss.str());
+			Mat img;
+      strToMat(res[i][0].as<std::string>(), img);
+      cvtColor(img, img, CV_BGR2GRAY);
+      threshold(img, img, 128, 255, 0);
 
-    Mat orig_img, img;
-    strToMat(res[0][0].as<string>(), orig_img);
+      Point pix = firstBlackLeft(img);
+      Point pix2 = firstBlackRight(img);
+      Point highest = highestLine(img);
+      int wperc = whitePercent(img);
+      inputs.at<float>(i,0) = pix.x;
+      inputs.at<float>(i,1) = pix.y;
+      inputs.at<float>(i,2) = pix2.x;
+      inputs.at<float>(i,3) = pix2.y;
+      inputs.at<float>(i,4) = highest.x;
+      inputs.at<float>(i,5) = highest.y;
+      inputs.at<float>(i,6) = wperc;
+  }
+  Mat out;
+  brain->predict(inputs, out); 
+  for (int i = 0; i < numImages; ++i) {
+    cout << "Exp: [" << res[i][1] << ", " << res[i][2] << "]" << endl;
+
+		Mat outData = out.row(i);
+    cout << "Actual: " << outData << endl;
+
+    Mat orig_img, img, disp_img;
+    strToMat(res[i][0].as<string>(), orig_img);
     cvtColor(orig_img, img, CV_BGR2GRAY);
     threshold(img, img, 128, 255, 0);
 
     Point pix = firstBlackLeft(img);
     Point pix2 = firstBlackRight(img);
     Point highest = highestLine(img);
-    int wperc = whitePercent(img);
-
-    Mat inData = (Mat_<double>(1,7, CV_32F) << pix.x, pix.y, pix2.x, pix2.y, highest.x, highest.y, wperc);
-
-    cout << "Exp: ["
-      << res[0][1].as<int>() << ", "
-      << res[0][2].as<int>() << ", "
-      << res[0][3].as<int>() << ", "
-      << res[0][4].as<int>() << ", "
-      << res[0][5].as<int>() << ", "
-      << res[0][6].as<int>() << "]" << endl;
-
-    Mat out;
-    brain->predict(inData, out);
-    double* row = out.ptr<double>(0);
-    cout << "Actual: ["
-        << row[0] << ", "
-        << row[1] << ", "
-        << row[2] << ", "
-        << row[3] << ", "
-        << row[4] << ", "
-        << row[5] << "]" << endl << endl;;
-
-    Mat disp_img;
     cvtColor(img, disp_img, CV_GRAY2BGR);
     circle(disp_img, pix, 10, CV_RGB(0,255,0), 5);
     circle(disp_img, pix2, 10, CV_RGB(0,255,0), 5);
